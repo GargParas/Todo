ceac03741f92a4b471c14bee89bfd73d88229695
Paras Garg <91370703+GargParas@users.noreply.github.com>
2022-10-05 19:13:41 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import './App.css';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import Footer from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
+  }
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
+  }
+
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return ( 
+    <> 
+    <BrowserRouter>
+      
+      <Routes>
+          <Route path="/" element={
+            <>
+            <Header title="My Todos List" searchBar={false} /> 
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} /> 
+            <Footer />
+            </>
+          }/> 
+         
+          <Route path='/about' element={<><Header title="My Todos List" searchBar={false} /> <About /> <Footer /></>}/>
+            
+        </Routes> 
+      
+    </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Footer() {
+
+  
+
+  return (
+    <footer className='bg-dark text-light py-2'>
+      <p className='text-center'>Copyright &copy; www.paramsoftware.com</p>
+
+    </footer>
+  )
+}
